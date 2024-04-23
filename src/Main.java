c44f723c16969de9c1ff0e78d46d2911ad676df2
Student <elkadjk@.pl>
2024-04-23 15:07:35 +0200

    pierwszy commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(24);
+        list.add(25);
+
+        list.sort((a,b) -> a-b);
+
+//        list.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return 0;
+//            }
+//        });
+
+        list.sort((o1,o2) -> o2-o1);
     }
 }
